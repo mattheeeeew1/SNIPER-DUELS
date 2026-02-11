7812bc0d5d05ae73546471282b81d62196eb7db0
mattheeeeew1 <146095616+mattheeeeew1@users.noreply.github.com>
2026-02-11 13:15:34 +0800

    Update SNIPER DUELS.lua

--- SNIPER DUELS.lua
+++ SNIPER DUELS.lua
@@ -14,8 +14,8 @@ local STORAGE_NAME = "System_Render_Cache"
 
 local AIM_PART = "UpperTorso"
 local AIM_SMOOTHNESS = 1
-local MAX_AIM_DISTANCE = 400
-local AIM_FOV = 60
+local MAX_AIM_DISTANCE = 500
+local AIM_FOV = 100
 local AIM_OFFSET = Vector3.new(0, 0, 0)
 
 local function GetStorage()
